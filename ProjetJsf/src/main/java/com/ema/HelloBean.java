534ef932b8167b53f6594a9228c353853d842a4a
bouzzo <anas_yz@hotmail.fr>
2015-11-22 00:05:59 +0100

    Commit de la v2 proprement configurée

---
+++ ProjetJsf/src/main/java/com/ema/HelloBean.java
@@ -0,0 +1,33 @@
+package com.ema;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
